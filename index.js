23c16e67a3def0b8cd8081c79f1b4cd18fed4f0b
Jan Kouřil <jan.kouril@infis.cz>
2025-11-04 12:24:59 +0100

    0.3.2

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 //function tlacitko(){
-//    alert("Naše firma byla vytvořena jako start-Up projekt v roce 2025 žáky třídy 2.E ve školním roce 2025/26")
+//    alert("Naše firma byla vytvořena jako start-Up projekt žáky třídy 2.E ve školním roce 2025/26")
 //}
